73b5a5d0dd69f7b1ca4e2fe387dc5c3742ec4a03
Sithija Lakshan <sithikaru@gmail.com>
2025-02-16 09:43:16 +0530

    Refactor Firebase configuration to use environment variables for improved security

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7QTphzIOGuXL3luGQaSliQaI0dx_oKsg",
-    authDomain: "skillup-ed290.firebaseapp.com",
-    projectId: "skillup-ed290",
-    storageBucket: "skillup-ed290.firebasestorage.app",
-    messagingSenderId: "492581200583",
-    appId: "1:492581200583:web:b3009418842a08e4c854e2"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
